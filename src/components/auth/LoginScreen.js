aa88780c24407da5689b225e364d28cd9711c9fb
cbazcode <cbazcode@gmail.com>
2020-11-30 20:52:36 -0500

    Login and Register with scss

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,33 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
     return (
-        <div>
-            <h3>LoginScreen</h3>
-            <form>
+      <>
+        <h3 className = 'auth__title'>LoginScreen</h3>
+        <form>
+          <input type="text" placeholder="Email" name="email" className = "auth__input" autoComplete = "off"/>
 
-                <input
-                    type = "text"
-                    placeholder ="email"
-                    name = "email"
-                
+          <input type="password" placeholder="Password" name="password" className = "auth__input" autoComplete = "off"/>
+
+          <button className="btn btn-primary btn-block" type="submit">Login</button>
+    
+          <div className = "auth__social-networks">
+            <p>Login with social network</p>
+            <div className="google-btn">
+              <div className="google-icon-wrapper">
+                <img
+                  className="google-icon"
+                  src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                  alt="google button"
                 />
-
-                <input
-                    type = "password"
-                    placeholder ="password"
-                    name = "password"
-                
-                />
-
-                <button
-                    type = "submit"
-                >
-                    Login
-                </button>
-                <hr/>
-                Google
-            </form>
-        </div>
-    )
+              </div>
+              <p className="btn-text">
+                <b>Sign in with google</b>
+              </p>
+            </div>
+          </div>
+          <Link to = "/auth/register"
+            className = "link"
+          >
+              Create new account
+          </Link>
+        </form>
+      </>
+    );
 }
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>RegisterScreen</h1>
-        </div>
+        <>
+        <h3 className = 'auth__title'>Register</h3>
+        <form>
+          <input type="text" 
+          placeholder="Name" 
+          name="name" 
+          className = "auth__input" 
+          autoComplete = "off"/>
+
+          <input type="text" 
+          placeholder="Email" 
+          name="email" 
+          className = "auth__input" 
+          autoComplete = "off"/>
+
+          <input type="password" placeholder="Password" 
+          name="password" 
+          className = "auth__input" 
+          />
+
+          <input type="password" placeholder="Confirm" 
+          name="password2" 
+          className = "auth__input" 
+          />
+
+          <button className="btn btn-primary btn-block mb-5" type="submit">Register</button>
+    
+          
+          <Link to = "/auth/login"
+            className = "link"
+          >
+              Already registered?
+          </Link>
+        </form>
+      </>
     )
 }
