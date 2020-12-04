5cc0b6153bfe496bd34411c58c4930167d529f1c
Sebastian Asis <sasisromero10@gmail.com>
2020-12-04 12:34:49 -0500

    Loading page was created and inserted in JournalApp

---
+++ src/components/loading/LoadingScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const LoadingScreen = () => {
+    return (
+        <div className="loading__container-loader">
+            <div className="loading__loader">
+                <div className="loading__loading_1"></div>
+                <div className="loading__loading_2">Espere...</div>
+            </div>       
+        </div>
+    )
+}
